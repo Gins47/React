325efd2ee0680a08dbccd1f4e054ca87e75d65f0
Gins <ginspcyriac1993@gmail.com>
2020-08-10 23:18:33 +0100

    First day create react app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # React
+1. Create React App
+
+  create-react-app react-complete-guide --script-version 1.1.5
+ 
+  cd react-complete-guide/
+
+  npm start
---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Person from './Components/Person'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>My First React App</p>
+      <Person />
+    </div>
+  );
+}
+
+export default App;
